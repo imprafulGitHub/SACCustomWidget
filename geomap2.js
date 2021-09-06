fb3fa6ad43f35cd35a66eeddb02024cbc07f3664
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-06 10:58:48 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -69,9 +69,13 @@
             let template = document.createElement("template");
             template.innerHTML = `
               <script>
-	       @import "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js";
+	        @import "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js";
 	       	@import "https://cdn.jsdelivr.net/npm/dat.gui@0.7.6/build/dat.gui.min.js";
 		@import "https://cdn.jsdelivr.net/npm/gcoord@0.2.3/dist/gcoord.js";
+		@import "https://maptalks.org/maptalks.three/demo/js/maptalks.js";
+		@import "https://cdn.jsdelivr.net/npm/three@0.104.0/build/three.min.js";
+		@import "https://cdn.jsdelivr.net/npm/maptalks.three@latest/dist/maptalks.three.js";
+		@import "https://cdn.jsdelivr.net/npm/three@0.104.0/examples/js/libs/stats.min.js";
 	      </script>
   		      <style>
                  @import "https://cdn.jsdelivr.net/npm/maptalks/dist/maptalks.css";
@@ -308,9 +312,9 @@
                     let shadowRoot = this.attachShadow({ mode: "open" });
                     shadowRoot.appendChild(template.content.cloneNode(true));
 
-                    //setTimeout(function () {
-                    //    load("", shadowRoot.getElementById("map"));
-                   // }, 3000);
+                    setTimeout(function () {
+                        load("", shadowRoot.getElementById("map"));
+                    }, 3000);
 
                 }
             }
