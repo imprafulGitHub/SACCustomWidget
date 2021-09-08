e9a953f83c2f1c2a39b048118944457bbc3a85f5
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-08 15:37:18 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -305,9 +305,9 @@
                     let shadowRoot = this.attachShadow({ mode: "open" });
                     shadowRoot.appendChild(template.content.cloneNode(true));
 
-                    setTimeout(function () {
-                        load("", shadowRoot.getElementById("map"));
-                    }, 3000);
+                    //setTimeout(function () {
+                    //    load("", shadowRoot.getElementById("map"));
+                   // }, 3000);
 
                 }
             }
