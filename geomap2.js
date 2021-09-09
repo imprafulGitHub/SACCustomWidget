23d1ba8cee18e6306bc28272a90f31aeffc5ccb6
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-09 09:37:55 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -282,7 +282,7 @@
                     let shadowRoot = this.attachShadow({ mode: "open" });
                     shadowRoot.appendChild(template.content.cloneNode(true));
                     
-		    var prop = '{"type":"FeatureCollection","features":[' +
+		   // var prop = '{"type":"FeatureCollection","features":[' +
                         '{"type": "Feature", "properties": { "City": "New York", "Country": "US", "Contract": "30000033", "ZipCode": "10059", "Amount": "78.68" }, "geometry": { "type": "Point", "coordinates": [113.950375, 22.534875] } },' +
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"88.68"}, "geometry": { "type":"Point", "coordinates": [113.950625, 22.534875] } },' +
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"98.68"}, "geometry": { "type":"Point", "coordinates": [113.930625, 22.516125] } },' +
@@ -293,6 +293,9 @@
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"88.68"}, "geometry": { "type":"Point", "coordinates": [113.929625, 22.515625] } },'+
                         '{"type":"Feature","properties":{"City":"New York","Country":"US","Contract":"30000033","ZipCode":"10059","Amount":"98.68"},"geometry":{"type":"Point","coordinates":[114.151875,22.555125]}}]}';
 		    
+			var prop = '{"type":"FeatureCollection","features":[' + info ;
+			
+			
                     setTimeout(function () {
                         load(prop, shadowRoot.getElementById("map"));
                     }, 3000);
