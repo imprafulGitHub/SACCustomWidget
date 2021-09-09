1ac78f9d4e75c8fcb1361a342e224183e72344d9
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-09 13:39:26 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -318,7 +318,10 @@
 			
 			//var data = '{"type":"FeatureCollection","features":[' + this.$info.slice(0, -3) + "]}'";
 			//this.render(this.$value, this.$info, this.$color);
-			console.log("Json Data - " + this.$value + " - " + this.$info + " - " + this.$color);
+			if(this.$info != null){
+				console.log("Json Data - " + this.$value + " - " + this.$info + " - " + this.$color);
+			}
+			
 			
 		}   
             }
