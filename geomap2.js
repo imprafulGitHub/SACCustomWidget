3b5694737519f65058ccb02a78b5f700cf919a1f
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-09 10:20:52 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -73,7 +73,7 @@
             function load(prop, ele) {
                 if (!initCalled) {
                     initCalled = true;
-                    console.log("Data - " + JSON.parse(prop));
+                    //console.log("Data - " + JSON.parse(prop));
 
                     map = new maptalks.Map(ele, {
                         "center": [113.98448073352165, 22.53682833203598],
@@ -282,7 +282,7 @@
                     let shadowRoot = this.attachShadow({ mode: "open" });
                     shadowRoot.appendChild(template.content.cloneNode(true));
                     
-		   // var prop = '{"type":"FeatureCollection","features":[' +
+		    var prop = '{"type":"FeatureCollection","features":[' +
                         '{"type": "Feature", "properties": { "City": "New York", "Country": "US", "Contract": "30000033", "ZipCode": "10059", "Amount": "78.68" }, "geometry": { "type": "Point", "coordinates": [113.950375, 22.534875] } },' +
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"88.68"}, "geometry": { "type":"Point", "coordinates": [113.950625, 22.534875] } },' +
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"98.68"}, "geometry": { "type":"Point", "coordinates": [113.930625, 22.516125] } },' +
@@ -293,8 +293,8 @@
                         '{"type":"Feature", "properties": { "City":"New York", "Country":"US", "Contract":"30000033", "ZipCode":"10059", "Amount":"88.68"}, "geometry": { "type":"Point", "coordinates": [113.929625, 22.515625] } },'+
                         '{"type":"Feature","properties":{"City":"New York","Country":"US","Contract":"30000033","ZipCode":"10059","Amount":"98.68"},"geometry":{"type":"Point","coordinates":[114.151875,22.555125]}}]}';
 		    
-			var prop = '{"type":"FeatureCollection","features":[' + info ;
-			
+			//var prop = '{"type":"FeatureCollection","features":[' + this.$info ;
+			console.log("Json Data - " + this.$info);
 			
                     setTimeout(function () {
                         load(prop, shadowRoot.getElementById("map"));
