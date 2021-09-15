8f17d72fd181b8bcddb6cc494ec10444bb697aee
imprafulGitHub <81234165+imprafulGitHub@users.noreply.github.com>
2021-09-15 15:34:53 +0530

    Update geomap2.js

--- geomap2.js
+++ geomap2.js
@@ -332,7 +332,7 @@
 			if ("color" in changedProperties) {
 				this.$color = changedProperties["color"];
 			}
-			LoadScript();
+			await LoadScript();
 			console.log("result change of geo 2");
 			if(this.$info != null && this.$info != '' && this.$info != undefined)
 			{
